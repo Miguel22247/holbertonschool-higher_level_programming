e12dedd0820ee54a63451c76be5b76eefd65ba96
Miguel22247 <minipachru@gmail.com>
2021-04-26 10:29:14 -0300

    added c file and header

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+* check_cycle - function that checks if list has cycle
+* @list: pointer to list to check
+* Return: On success 1
+* On error, -1 and errno
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *temp;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	while (list)
+	{
+		temp = list;
+		list = list->next;
+
+		if (temp <= list)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
