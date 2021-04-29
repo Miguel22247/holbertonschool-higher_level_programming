acc4923696860ea25b907ed59c593e5c5b9f049f
Miguel22247 <nr2862002@gmail.com>
2021-04-29 10:16:46 -0300

    fixed pep8

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-	result = 0
-	for argument in range(1, len(argv)):
-		result = result + int(argv[argument])
-	print("{}".format(result))
+    result = 0
+    for argument in range(1, len(argv)):
+        result = result + int(argv[argument])
+    print("{}".format(result))
