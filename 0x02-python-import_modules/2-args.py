4069d1c9384f0541110030457933c83239d77119
Miguel22247 <nr2862002@gmail.com>
2021-04-29 13:04:38 -0300

    fixed checker

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(len(argv) - 1))
         for argument in range(1, len(argv)):
-            print("{} : {}".format(argument, argv[argument]))
+            print("{}: {}".format(argument, argv[argument]))
