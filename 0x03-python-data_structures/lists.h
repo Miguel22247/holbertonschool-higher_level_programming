017a99f08b8c33c86349c0eb5cfa884da110f0ce
Miguel22247 <nr2862002@gmail.com>
2021-05-04 13:02:44 -0300

    added libraries to header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,10 @@
+
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -21,4 +25,3 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
