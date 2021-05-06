923a34aaf8dcb4c5f932bacf4742c0644b6c733a
Miguel22247 <nr2862002@gmail.com>
2021-05-06 11:34:57 -0300

    added the first six python files

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_i = []
+        new_i.append(new_i)
+        for j in i:
+            new_i.append(j ** 2)
+return new_matrix
---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+	a = set_1
+	b = set_2
+	return a & b
