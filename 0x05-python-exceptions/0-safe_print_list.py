74c558a1a436a2065a94985c48d11f732a3c57b9
Miguel22247 <nr2862002@gmail.com>
2021-05-17 10:18:40 -0300

    Fixed pep8 style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range (0, x):
+        for i in range(0, x):
             print("{}".format(my_list[i]), end=" ")
             print("")
     except IndexError:
