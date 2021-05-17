612f4592c391643e45d95a304ba93522d5508e8a
Miguel22247 <nr2862002@gmail.com>
2021-05-17 09:37:25 -0300

    Added file 3 to repo

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = a / b
+    except ZeroDivisionError:
+        c = None
+        return
+    finally:
+        print("Inside result: {}".format(c))
+        return c
