a63343f6bf6601a9d50c94cbf0644a0618dcd347
Miguel22247 <nr2862002@gmail.com>
2021-05-18 10:26:05 -0300

    added file 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """property to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter to set it"""
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size
+    
+    def _print(self):
+        """Public instance method that prints the square with the char #"""
+        if self.__size > 0:
+            for x in range(0, self.__size):
+                for y in range(0, self.__size):
+                    print("#", end ="")
+                print("")
+        else:
+            print("")
