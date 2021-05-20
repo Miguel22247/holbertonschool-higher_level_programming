a0475bf7b604766817b5a56c50d89a8387bfa1ec
Miguel22247 <nr2862002@gmail.com>
2021-05-20 10:25:40 -0300

    fixed pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,16 +5,16 @@ a and b must be integers or floats, otherwise raise a TypeError exception.
 Returns an integer: the addition of a and b."""
 
 def add_integer(a, b=98):
-	"""b is always 98 unless specified otherwise
+    """b is always 98 unless specified otherwise
     a and b must be first casted to integers if they are float
     Returns an integer: the addition of a and b"""
 
-	if type(a) is not int and type(a) is not float:
-		raise TypeError('a must be an integer')
-	if type(b) is not int and type(b) is not float:
-		raise TypeError('b must be an integer')
-	if type(a) is float:
-		a = int(a)
-	if type(b) is float:
-		b = int(b)
-	return a + b
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
