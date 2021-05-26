b0294558059ccfc52992959a835d286c9d47ece4
Miguel22247 <nr2862002@gmail.com>
2021-05-26 16:27:34 -0300

    fixed comments

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,19 +7,19 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max(self):
-        """Test the max int on ints"""
+        """Test the max integer on integers"""
         self.assertEqual(max_integer(list=[0, 1, 2, 3]), 3)
         self.assertEqual(max_integer(list=[2, 3, 1, 0]), 3)
         self.assertEqual(max_integer(list=[-1000, 0, -4, -150]), 0)
         self.assertEqual(max_integer(list=[-100, -110, -400, -150]), -100)
 
     def test_empty(self):
-        """Test if list is empty"""
+        """Test if the list is empty"""
         self.assertIsNone(max_integer(list=[None]))
         self.assertIsNone(max_integer(list=[]))
 
     def test_errors(self):
-        """Test possible errors"""
+        """Test possible errors in the code"""
         self.assertRaises(TypeError, max_integer, list=[2, "Hello", 3])
         self.assertRaises(TypeError, max_integer, list=[[1, 2, 3], 100, 100.6])
         self.assertRaises(TypeError, max_integer, list=[(1, 2), 100, 100.6])
