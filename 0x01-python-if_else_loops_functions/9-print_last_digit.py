67b912c0029fa0c94e30989be7464f2c5f08ae87
Miguel22247 <nr2862002@gmail.com>
2021-05-26 16:15:31 -0300

    returned to previous state

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if num < 0:
-        num = num * -1
-    print("{:d}".format(num % 10), end="")
-    return num % 10
+    if number < 0:
+        number = number * -1
+    print("{:d}".format(number % 10), end="")
+    return number % 10
