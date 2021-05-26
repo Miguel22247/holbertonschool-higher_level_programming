36fc99a44e545178386f2ca761af6a73e5f690c4
Miguel22247 <nr2862002@gmail.com>
2021-05-26 16:34:48 -0300

    fixed things

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,9 +8,9 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         """Test the max integer on integers"""
-        self.assertEqual(max_integer(list=[0, 1, 2, 3]), 3)
-        self.assertEqual(max_integer(list=[2, 3, 1, 0]), 3)
-        self.assertEqual(max_integer(list=[-1000, 0, -4, -150]), 0)
+        self.assertEqual(max_integer(list=[7, 10, 22, 36]), 3)
+        self.assertEqual(max_integer(list=[2, 39, 1, 10]), 3)
+        self.assertEqual(max_integer(list=[-1000, 0, -42, -150]), 0)
         self.assertEqual(max_integer(list=[-100, -110, -400, -150]), -100)
 
     def test_empty(self):
