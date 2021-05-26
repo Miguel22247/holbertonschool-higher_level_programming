ceb1a19862a1e28b1d7c09573cbdd12cf660909f
Miguel22247 <nr2862002@gmail.com>
2021-05-26 16:26:33 -0300

    fixed things

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Test file for max_integer([..]) exercise
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
@@ -12,22 +12,17 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer(list=[2, 3, 1, 0]), 3)
         self.assertEqual(max_integer(list=[-1000, 0, -4, -150]), 0)
         self.assertEqual(max_integer(list=[-100, -110, -400, -150]), -100)
-        self.assertEqual(max_integer(list=[5, 5, 5]), 5)
-        self.assertEqual(max_integer(list=[100.5, 100.4, 100.6]), 100.6)
-        self.assertEqual(max_integer(list=[100.5, 100, 100.6]), 100.6)
 
     def test_empty(self):
         """Test if list is empty"""
         self.assertIsNone(max_integer(list=[None]))
         self.assertIsNone(max_integer(list=[]))
-        self.assertIsNone(max_integer())
 
     def test_errors(self):
         """Test possible errors"""
         self.assertRaises(TypeError, max_integer, list=[2, "Hello", 3])
         self.assertRaises(TypeError, max_integer, list=[[1, 2, 3], 100, 100.6])
         self.assertRaises(TypeError, max_integer, list=[(1, 2), 100, 100.6])
-        self.assertRaises(TypeError, max_integer, None)
 
 if __name__ == '__main__':
     unittest.main()
