98d55d243b9f7db29cdd6090d7945f82f44dc8bd
Miguel22247 <nr2862002@gmail.com>
2021-06-01 20:06:31 -0300

    added a note

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,11 @@
 def inherits_from(obj, a_class):
     """Function that inherits"""
 
-    if type(obj) is a_class or not isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
-        return False
+        return isinstance(obj, a_class)
+
+"""form that doesn't pass the checker 
+if type(obj) is a_class or not isinstance(obj, a_class):
+"""
