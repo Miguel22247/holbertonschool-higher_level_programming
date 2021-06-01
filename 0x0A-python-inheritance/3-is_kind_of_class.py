dcd804ea2470493b0f03f58576176a12ec82bcfb
Miguel22247 <nr2862002@gmail.com>
2021-06-01 11:33:11 -0300

    added file 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""returns True if the object or false"""
+
+
+def is_kind_of_class(obj, a_class):
+	""""Function to check if return true or"""
+	if isinstance(obj, a_class):
+		return True
+	else:
+		return False
