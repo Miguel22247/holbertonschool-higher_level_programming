a6800c15f719091d6c3a62103215456ae676c2ae
Miguel22247 <nr2862002@gmail.com>
2021-06-01 11:41:41 -0300

    added 6 file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function to create basegeometry"""
+
+
+class BaseGeometry:
+	"""class that contains function area"""
+
+	def area(self):
+		"""Function not implemented"""
+        raise Exception("area() is not implemented")
