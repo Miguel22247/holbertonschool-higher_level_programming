b29efa4c5324c3b2e2067afe921095bde94122c0
Miguel22247 <nr2862002@gmail.com>
2021-06-01 11:38:09 -0300

    add file 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function that inherits"""
+
+
+def inherits_from(obj, a_class):
+	"""Function that inherits"""
+
+	if type(obj) is a_class or not isinstance(obj, a_class):
+		return True
+	else:
+		return False
