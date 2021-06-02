16886beb27b43c8a4d94c96740df093b7cb99eef
Miguel22247 <nr2862002@gmail.com>
2021-06-02 10:35:12 -0300

    FIXED pep8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ import sys
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
+
 try:
     loadFile = load_from_json_file("add_item_json")
 except FileNotFoundError:
