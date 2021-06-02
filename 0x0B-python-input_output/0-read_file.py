6f3e3aac35dc37a710434b0eb9621ed2f1ac1b76
Miguel22247 <nr2862002@gmail.com>
2021-06-02 09:13:18 -0300

    added file 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that read files on UTF-8"""
+
+
+def read_file(filename=""):
+	"""Read the file on UTF-8"""
+
+
+	with open(filename, enconding="utf-8") as readFile:
+		print(readFile.read(), end="")
