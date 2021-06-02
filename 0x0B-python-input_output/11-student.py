5d048ca4eaf65af4a305b0276df13c245a566b7c
Miguel22247 <nr2862002@gmail.com>
2021-06-02 11:03:53 -0300

    added file 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """Contain student data"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionary"""
+
+
+        if attrs == None or type(attrs) != list: 
+                return self.__dict__
+        else:
+            temp_list = {}
+            for element in attrs:
+                if type(element) is not str:
+                    return self.__dict__
+                if element in self.__dict__.keys():
+                    temp_list[element] = self.__dict__[element]
+            return temp_list
+
+    def reload_from_json(self, json):
+        """Replace all items on json"""
+
+        for items in json.keys():
+            self.__dict__[items] = json[items]
