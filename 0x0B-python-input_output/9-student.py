376354076edebc4e97cedc37eecccbfe8af63638
Miguel22247 <nr2862002@gmail.com>
2021-06-02 10:46:04 -0300

    added file 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """Contain studen data"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """return a dictionary"""
+
+
+        return self.__dict__
