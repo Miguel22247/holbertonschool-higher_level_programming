429fc330f3a4d111db1bf85160bc08e521981996
Miguel22247 <nr2862002@gmail.com>
2021-06-02 09:28:08 -0300

    added file 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that write a file on UTF-8"""
+
+
+def write_file(filename="", text=""):
+
+
+    with open(filename, mode ="w", enconding="utf-8") as writeFile:
+        writeFile.write(text)
+        return len(text)
