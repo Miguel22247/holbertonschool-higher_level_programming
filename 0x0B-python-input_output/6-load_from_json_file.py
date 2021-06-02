bbf065e8859c58aa63de171405856160edcd83db
Miguel22247 <nr2862002@gmail.com>
2021-06-02 10:31:20 -0300

    added files 6 & 7

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""load from a json file"""
+import json
+
+def load_from_json_file(filename):
+    """Returns created object from a JSON file
+    """
+
+    with open(filename, mode="r", encoding="UTF-8") as readFile:
+       return json.load(readFile) 
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""add an item"""
+import sys
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+try:
+	loadFile = load_from_json_file("add_item_json")
+except FileNotFoundError:
+	loadFile = []
+
+argc = len(sys.argv)
+for index in range (1, argc):
+	loadFile.append(sys.argv[index])
+save_to_json_file(loadFile, "add_item.json")
