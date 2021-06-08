92f1be911ce885c848237ca69bcc6c21d6d83c09
Miguel22247 <minipachru@gmail.com>
2021-06-08 13:49:19 -0300

    rewrited base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,49 +14,48 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns a JSON string representation"""
+        """Returns JSON string representation"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string repr to file"""
+        new_list = []
+        filename = cls.__name__ + '.json'
+        if list_objs is not None:
+            for i in range(len(list_objs)):
+                new_list.append(list_objs[i].to_dictionary())
+        jstring = cls.to_json_string(new_list)
+        with open(filename, 'w') as my_file:
+            my_file.write(jstring)
+
     @staticmethod
     def from_json_string(json_string):
-        """Returns JSON strings in list"""
-        if type(json_string) is not str or len(json_string) == 0:
+        """Returns the list of the JSON string representation"""
+        if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Returns an instance with all attrs already set"""
-        if cls.__name__ is "Rectangle":
-            temp = cls(1, 2)
-        if cls.__name__ is "Square":
-            temp = cls(3)
-        # update temp with obj func update()
-        temp.update(**dictionary)
-        return temp
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Writes to file with JSON string"""
-        with open(cls.__name__ + ".json", mode="w") as write_file:
-            if list_objs is None:
-                write_file.write("[]")
-            else:
-                # Using to_json_string(), and to_dictionary() to format
-                write_file.write(cls.to_json_string(
-                                 [item.to_dictionary() for item in list_objs]))
+        """Returns instance with all attributes already set"""
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 2)
+        elif cls.__name__ == 'Square':
+            dummy = cls(3)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
-        """Returns a list of instances"""
+        """Return list of instances"""
         filename = cls.__name__ + '.json'
         my_list = []
-        if os.path.exists(filename):
+        if os.path.isfile(filename):
             with open(filename, 'r') as my_file:
                 File = my_file.read()
                 new_list = cls.from_json_string(File)
