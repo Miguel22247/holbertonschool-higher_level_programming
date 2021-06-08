2e29343f5df8fbf875993a4774490bae74bd67f0
Miguel22247 <minipachru@gmail.com>
2021-06-08 14:21:55 -0300

    fixed pep8 on line 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns JSON string representation"""
