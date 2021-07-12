e36a0e8d5c7a1484c47dd7f44edf6f2e71f7371c
Miguel22247 <minipachru@gmail.com>
2021-07-12 13:03:01 -0300

    fixed 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- insert a new row
 -- in the table first_table
-INSERT INTO first_table (id, name) VALUES ('89', 'Holberton');
+INSERT INTO first_table (id, name) VALUES ('89', 'Holberton School');
