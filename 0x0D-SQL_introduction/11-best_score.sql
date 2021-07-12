29d4b32f13c4c4dc358ed31b7f29e94844e30244
Miguel22247 <minipachru@gmail.com>
2021-07-12 14:59:00 -0300

    added conditional to order by

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists all records
 -- score >= 10
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
