ac3eda86e3c7a3f1d371a85fe5e17f7bc1394c13
Miguel22247 <nr2862002@gmail.com>
2021-07-13 15:42:01 -0300

    rewrited

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,8 +2,8 @@
 -- of the database
 SELECT tv_shows.title FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_show_genres.show_id=tv_shows.id
+ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genre.name = 'Comedy'
+WHERE tv_genres.name='Comedy'
 ORDER BY tv_shows.title;
