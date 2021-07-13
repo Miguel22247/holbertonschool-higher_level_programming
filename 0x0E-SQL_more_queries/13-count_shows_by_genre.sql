51107da764a97ae1b8592c3903c7cdf864f5d626
Miguel22247 <nr2862002@gmail.com>
2021-07-13 13:45:32 -0300

    added coma

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows 
 -- in the database
-SELECT tv_genres.name AS genre COUNT(tv_genres.id) AS number_of_shows FROM tv_shows
+SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_of_shows FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_show_genres.show_id=tv_genres.id
 GROUP BY genre
