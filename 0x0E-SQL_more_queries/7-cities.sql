62fb9e0b7a9628ad9ff35d5899b03703f69778cc
Miguel22247 <nr2862002@gmail.com>
2021-07-13 12:07:03 -0300

    added 6-7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- create the database
+-- and tables
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE hbtn_0d_usa.cities (
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL
+	PRIMARY KEY(id)),
+	FOREIGN KEY(state_id) REFERENCES states(id));
