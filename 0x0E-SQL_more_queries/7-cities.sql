0a08e99a02b1a8355b4455c2deaaa524f83d4383
Miguel22247 <nr2862002@gmail.com>
2021-07-13 18:38:42 -0300

    changed some things in 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,7 @@
 -- and tables
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES states(id));
+	state_id INT NOT NULL REFERENCES(states(id)),
+	name VARCHAR(256) NOT NULL;
