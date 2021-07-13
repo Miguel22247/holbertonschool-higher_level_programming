76c2a082e3a8ff2c2a77c44801ba7928bde1df64
Miguel22247 <nr2862002@gmail.com>
2021-07-13 19:02:04 -0300

    ordered code

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,7 @@
 -- lists all genres
 -- of the database
-SELECT tv_genres.name FROM tv_genres LEFT JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
+SELECT tv_genres.name FROM tv_genres 
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id 
+LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_shows.title = 'Dexter' 
+ORDER BY tv_genres.name;
