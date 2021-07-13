bde2bde1de3c0377280f28f7ee2b8c1fc5984e83
Miguel22247 <nr2862002@gmail.com>
2021-07-13 13:58:10 -0300

    changed ON condition

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- in the database
 SELECT tv_genres.name AS genre, COUNT(tv_genres.id) AS number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres
-ON tv_show_genres.show_id=tv_genres.id
+ON tv_show_genres.genre_id=tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
 
