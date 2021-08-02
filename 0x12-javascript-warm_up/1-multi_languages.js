6812b24b0a981274de5809d35205d4721e536201
Miguel22247 <minipachru@gmail.com>
2021-08-02 12:43:13 -0300

    fixed?

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const c_print = 'C is fun';
-const python_print = 'Python is cool';
-const javascript_print = 'JavaScript is amazing';
+const CPrint = 'C is fun';
+const PythonPrint = 'Python is cool';
+const JavascriptPrint = 'JavaScript is amazing';
 console.log(c_print);
 console.log(python_print);
 console.log(javascript_print);
