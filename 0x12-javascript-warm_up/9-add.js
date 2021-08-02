3435db77939b0501f956938ee3ef91d5553e1d51
Miguel22247 <minipachru@gmail.com>
2021-08-02 17:02:26 -0300

    rewrited 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const Number1 = Number(argv[2]);
-const Number2 = Number(argv[3]);
+const Argv1 = process.argv;
+const Argv2 = process.argv;
+const number1 = Number(Argv1[2]);
+const number2 = Number(Argv2[3]);
 
 function add (a, b) {
-  const suma = Number1 + Number2;
-  console.log(suma);
+  const sum = number1 + number2;
+  console.log(sum);
 }
 
-add(Number1, Number2);
+add(number1, number2);
