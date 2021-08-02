662f30ebd8d0b7b5fcc82e55702f30169ed1e6cb
Miguel22247 <minipachru@gmail.com>
2021-08-02 13:16:15 -0300

    fixed semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // prints the first argument
-const arg_count = [];
+const ArgCount = [];
 let ind;
 process.argv.forEach((val, index) => {
-	arg_count[index] = `${val}`;
+	ArgCount[index] = `${val}`;
 	ind  = `${index}`;
 });
 if (ind < 2) {
 	console.log ('No argument');
 } else {
-	console.log (arg_count[2]);
+	console.log (ArgCount[2]);
 }
