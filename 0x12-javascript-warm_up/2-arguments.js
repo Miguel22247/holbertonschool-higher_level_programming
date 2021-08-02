3e417d2f3ae184f96f256277d29a3b920ba0b957
Miguel22247 <minipachru@gmail.com>
2021-08-02 12:48:18 -0300

    fixed?

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 // This script prints using arguments
 const argv_length = process.argv.length;
 
-if (argv_length < 3) {
-  console.log('No arguments');
+if (argv_length === 3) {
+  console.log('No argument');
 } else if (argv_length === 1) {
-  console.log('Argument found');
+  console.log('Arguments found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
