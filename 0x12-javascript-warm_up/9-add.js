32fbe2050540726a95bedaccb22cc8ce23354b1c
Miguel22247 <minipachru@gmail.com>
2021-08-02 16:46:49 -0300

    fixed semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,8 +3,8 @@ const argv = require('process');
 const Number1 = Number(argv[2]);
 const Number2 = Number(argv[3]);
 
-function add(a,b) {
+function add(a, b) {
   const suma = Number1 + Number2;
-  console.log (suma);
+  console.log(suma);
 }
-add (Number1, Number2);
+add(Number1, Number2);
