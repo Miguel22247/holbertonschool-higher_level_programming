4ef9963eb8275b70a4514b84c9115e37d83b842e
Miguel22247 <minipachru@gmail.com>
2021-08-02 15:56:39 -0300

    added 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg = [];
+let x;
+let square = 'X';
+process.argv.forEach((val, index) => {
+	arg[index] = `${val}`;
+});
+const number = Number(`${arg[2]}`);
+if (isNaN(number)) {
+	console.log('Missing size');
+} else {
+	console.log(square.repeat(number));
+}
