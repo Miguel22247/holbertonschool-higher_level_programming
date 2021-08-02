1cdaf1d3919b6fcd0b9152f0fb9b68c8281c3dd9
Miguel22247 <minipachru@gmail.com>
2021-08-02 10:04:03 -0300

    fixed variable names

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 let c_print = "C is fun";
 let python_print = "Python is cool";
 let javascript_print = "JavaScript is amazing";
-console.log (C_print);
+console.log (c_print);
 console.log (python_print);
 console.log (javascript_print);
