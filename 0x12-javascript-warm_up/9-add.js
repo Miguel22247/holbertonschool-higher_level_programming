ec63bf5378642e95dc831a4d515e62aeb38572f9
Miguel22247 <minipachru@gmail.com>
2021-08-02 16:44:43 -0300

    added 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = require('process');
+const Number1 = Number(argv[2]);
+const Number2 = Number(argv[3]);
+
+function add(a,b) {
+  const suma = Number1 + Number2;
+  console.log (suma);
+}
+add (Number1, Number2);
