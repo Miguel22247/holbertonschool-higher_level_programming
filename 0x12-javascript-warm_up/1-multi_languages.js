66a7a67437a104958dda373e1e6caa37cd72c7a6
Miguel22247 <minipachru@gmail.com>
2021-08-02 10:02:49 -0300

    added 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let c_print = "C is fun";
+let python_print = "Python is cool";
+let javascript_print = "JavaScript is amazing";
+console.log (C_print);
+console.log (python_print);
+console.log (javascript_print);
