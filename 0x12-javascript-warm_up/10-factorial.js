dfdba26b6f114f6a575d62832b32fff8a72114e5
Miguel22247 <minipachru@gmail.com>
2021-08-02 17:22:49 -0300

    fixed 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const arg = [];
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n)) {
     return (1);
-  }
-  else if (n === 1) {
+  } else if (n === 1) {
     return (1);
   } else {
     return (n * factorial(n - 1));
