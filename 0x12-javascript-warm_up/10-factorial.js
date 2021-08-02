22443e203c2cfe0ecd666adb1bf00d285ba86754
Miguel22247 <minipachru@gmail.com>
2021-08-02 17:12:41 -0300

    added 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const arg = [];
+function factorial(n) {
+  if (isNaN(n)) {
+	return (1);
+  }
+  else if (n === 1) {
+	return (1);
+  } else {
+  	return (n * factorial(n - 1)); 
+  }
+}
+process.argv.forEach((val, index) => {
+  arg[index] = `${val}`;
+});
+console.log(factorial(arg[2]));
