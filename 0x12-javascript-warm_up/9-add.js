7603fa5d9cf16ddf277ad832cf588e70301b5808
Miguel22247 <minipachru@gmail.com>
2021-08-02 17:03:41 -0300

    changed some things

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const Argv1 = process.argv;
-const Argv2 = process.argv;
-const number1 = Number(Argv1[2]);
-const number2 = Number(Argv2[3]);
+const Argv1 = process.argv[2];
+const Argv2 = process.argv[3];
+const number1 = Number(Argv1);
+const number2 = Number(Argv2);
 
 function add (a, b) {
   const sum = number1 + number2;
