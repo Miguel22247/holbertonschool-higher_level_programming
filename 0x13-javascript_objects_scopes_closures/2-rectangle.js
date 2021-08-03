2e978d403219602b87b5b943846ebaf404235df3
Miguel22247 <minipachru@gmail.com>
2021-08-03 11:36:47 -0300

    Rewrited module.export

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (h > 0 && w > 0) {
       this.width = w;
       this.height = h;
     }
+    if (!w || !h) {
+      return;
+    }
   }
 };
+
+module.exports = Rectangle;
