e3e723e36351de38edd6f4e442dbf6a5e1d34c50
Miguel22247 <minipachru@gmail.com>
2021-08-03 14:11:10 -0300

    fixed

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,8 +3,8 @@ exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
   for (let x = 0; x < list.length; x++) {
     if (list[x] === searchElement) {
-            counter++;
+      counter++;
     }
   }
   return (counter);
-}
+};
