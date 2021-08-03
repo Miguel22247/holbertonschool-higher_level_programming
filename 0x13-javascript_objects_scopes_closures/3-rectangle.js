3806131c3031c8a032061c3b233cdb90ca577491
Miguel22247 <minipachru@gmail.com>
2021-08-03 11:55:16 -0300

    fixed

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,11 +9,12 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     const print = 'X';
     for (let x = 0; x < this.height; x++) {
       console.log(print.repeat(this.width));
     }
   }
-};
+}
 module.exports = Rectangle;
