d3005f4ec8e93e7da604d13c023f540946de7364
Miguel22247 <minipachru@gmail.com>
2021-08-03 10:52:11 -0300

    fixed semistandard

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (h > 0 && w > 0) { 
-    this.width = w;
-    this.height = h;
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
