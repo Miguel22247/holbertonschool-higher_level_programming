dd7306f4b2b7f5dce300a3f6ff2d65cb9923df09
Miguel22247 <minipachru@gmail.com>
2021-08-03 13:27:12 -0300

    fixed

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,11 +6,11 @@ class Square extends Square1 {
     const Print = 'X';
     if (!c) {
       for (let x = 0; x < this.width; x++) {
-          console.log(c.repeat(this.height));
+          console.log(Print.repeat(this.height));
       }
     } else {
         for (let x = 0; x < this.width; x++) {
-            console.log(Print.repeat(this.width))
+            console.log(c.repeat(this.width))
 
       }
     }
