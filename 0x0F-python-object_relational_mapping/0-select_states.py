8f3a268b68fa781f344f9df2fddcd7b948b42e2e
Miguel22247 <minipachru@gmail.com>
2021-08-05 12:21:04 -0300

    added a variable that storages the SQL query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,10 @@ if __name__ == '__main__':
     MY_PSWD = argv[2]
     MY_DBNAME = argv[3]
     try:
-        databases = MySQLdb.\
+        database = MySQLdb.\
             connect(host=MY_HOST, user=MY_USER, password=MY_PSWD, db=MY_DBNAME)
-        cur = databases.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        cur = database.cursor()
+        query = 'SELECT * FROM states ORDER BY id ASC'
+        cur.execute(query)
     except Exception as excep:
         print("ERROR {}".format(excep))
