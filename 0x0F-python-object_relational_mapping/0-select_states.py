60ac6bb9b466248511ae240cc28117f44bfaac27
Miguel22247 <minipachru@gmail.com>
2021-08-05 12:39:46 -0300

    rewrited

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,17 +6,11 @@ if __name__ == '__main__':
     MY_HOST = 'localhost'
     MY_USER = argv[1]
     MY_PSWD = argv[2]
-    MY_DBNAME = argv[3]
-    try:
-        database = MySQLdb.\
-            connect(host=MY_HOST, user=MY_USER, password=MY_PSWD, db=MY_DBNAME)
-        cur = database.cursor()
-        query = 'SELECT * FROM states ORDER BY id ASC'
-        cur.execute(query)
-        row_query = cur.fetchall()
-        for rq_print in row_query:
-            print(rq_print)
-        cur.close()
-        database.close()
-    except Exception as excep:
-        print("ERROR {}".format(excep))
+    MY_DB = argv[3]
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, password=MY_PSWD, db=MY_DBN)
+    cur = db.cursor()
+    query = 'SELECT * FROM states ORDER BY id ASC'
+    cur.execute(query)
+    row_query = cur.fetchall()
+    for rq_print in row_query:
+        print(rq_print)
