4897f3b3bbae5192d1031b385b7cb7a7224529ff
Miguel22247 <minipachru@gmail.com>
2021-08-05 10:17:10 -0300

    fixed pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Script that lists all states"""
 if __name__ == '__main__':
-	import MySQLdb
-	from sys import argv
-	MY_HOST = 'localhost'
-	MY_USER = argv[[1]]
-	MY_PSWD = argv[2]
-	MY_DBNAME = argv[3]
-	
+    import MySQLdb
+    from sys import argv
+    MY_HOST = 'localhost'
+    MY_USER = argv[[1]]
+    MY_PSWD = argv[2]
+    MY_DBNAME = argv[3]
+    
