8f6922c34739b60ac685c536bf0a2464914a737e
Miguel22247 <minipachru@gmail.com>
2021-08-05 14:12:46 -0300

    added port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,12 @@ if __name__ == '__main__':
     import MySQLdb
     from sys import argv
     HOST = 'localhost'
+    PORT = 3306
     MY_USER = argv[1]
     MY_PSWD = argv[2]
     MY_DB = argv[3]
-    db = MySQLdb.connect(host=HOST, user=MY_USER, passwd=MY_PSWD, db=MY_DB)
+    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
+                         db=MY_DB, port=PORT)
     cur = db.cursor()
     query = 'SELECT * FROM states ORDER BY id'
     cur.execute(query)
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Script that lists all states starting with N"""
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    HOST = 'localhost'
+    PORT = 3306
+    MY_USER = argv[1]
+    MY_PSWD = argv[2]
+    MY_DB = argv[3]
+    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
+                         db=MY_DB, port=PORT)
+    cur = db.cursor()
