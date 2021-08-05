6ab17ec5fc9af5599be25261e5c0ae735142c8d5
Miguel22247 <minipachru@gmail.com>
2021-08-05 11:03:27 -0300

    added 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,12 @@ if __name__ == '__main__':
     import MySQLdb
     from sys import argv
     MY_HOST = 'localhost'
-    MY_USER = argv[[1]]
+    MY_USER = argv[1]
     MY_PSWD = argv[2]
     MY_DBNAME = argv[3]
-    
+    try:
+        databases = MySQLdb.\
+            connect(myhost = MY_HOST, user = MY_USER, password = MY_PSWD)
+        cur = databases.cursor()
+    except Exception as e:
+        print("ERROR")
