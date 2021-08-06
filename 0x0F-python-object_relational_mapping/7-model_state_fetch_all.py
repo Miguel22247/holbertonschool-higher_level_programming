c640c598a01916b5e2bafc46a0d69a95eb25f7dd
Miguel22247 <minipachru@gmail.com>
2021-08-06 14:12:36 -0300

    changed argv import

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,17 +2,18 @@
 """A script that lists all states"""
 
 from os import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("error")
     else:
-        USER = sys.argv[1]
-        PWD = sys.argv[2]
-        DB = sys.argv[3]
+        USER = argv[1]
+        PWD = argv[2]
+        DB = argv[3]
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                                 .format(USER, PWD, DB), pool_pre_ping=True)
         Session = sessionmaker(bind=engine)
