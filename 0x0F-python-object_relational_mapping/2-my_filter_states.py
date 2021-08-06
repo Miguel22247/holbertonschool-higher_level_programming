e41d15aef33f0775e7dc4a14b44c86b8d7db91c7
Miguel22247 <minipachru@gmail.com>
2021-08-06 09:02:42 -0300

    added 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-""" Script that lists all values"""
+""" Script that lists all states starting with N"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+
     HOST = 'localhost'
     PORT = 3306
     MY_USER = argv[1]
     MY_PSWD = argv[2]
     MY_DB = argv[3]
+    NAME = argv[4]
     db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
                          db=MY_DB, port=PORT)
     cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(NAME)
+    cur.execute(query)
+    row_query = cur.fetchall()
+    for rq_print in row_query:
+        if rq_print[1] == 'NAME':
+            print(rq_print)
+db.close()
