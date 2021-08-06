32408b07c03f4df5f3cc8e6122ea36d801d73319
Miguel22247 <minipachru@gmail.com>
2021-08-06 10:11:34 -0300

    ADDED 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that lists all states starting with N"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    HOST = 'localhost'
+    PORT = 3306
+    MY_USER = argv[1]
+    MY_PSWD = argv[2]
+    MY_DB = argv[3]
+    NAME = argv[4]
+    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
+                         db=MY_DB, port=PORT)
+    cur = db.cursor()
+    query = "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') \
+                    FROM cities \
+                    INNER JOIN states \
+                    ON states.name = %s \
+                    WHERE cities.state_id = states.id \
+                    ORDER BY cities.id ASC;", (NAME,)
+    cur.execute(query)
+    row_query = cur.fetchall()
+    for rq_print in row_query:
+        print(rq_print)
+db.close()
