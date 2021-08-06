9cdd48ea3061eb7cc13bc72dd2ab74f2a4994a6e
Miguel22247 <minipachru@gmail.com>
2021-08-06 09:56:03 -0300

    added 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Script that lists all states starting with N"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    HOST = 'localhost'
+    PORT = 3306
+    MY_USER = argv[1]
+    MY_PSWD = argv[2]
+    MY_DB = argv[3]
+    NAME = argv[4]
+    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
+                         db=MY_DB, port=PORT)
+    cur = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name \
+                    FROM cities \
+                    INNER JOIN states \
+                    ON cities.state_id = states.id \
+                    ORDER BY cities.id ASC;"
+    cur.execute(query)
+    row_query = cur.fetchall()
+    for rq_print in row_query:
+        print(rq_print)
+db.close()
