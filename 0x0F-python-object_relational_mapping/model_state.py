17bfb751f43ac9d09ff12d99962c103f2daccaac
Miguel22247 <minipachru@gmail.com>
2021-08-06 12:40:05 -0300

    added model states

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Starts the link with the database"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+	"""A class states that iterates from base"""
+	__tablename__ = 'states'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(128))
