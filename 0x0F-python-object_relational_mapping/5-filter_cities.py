c538ca19cf4ee62da4df8a9c1922982e923870de
Miguel22247 <minipachru@gmail.com>
2021-08-06 10:14:51 -0300

    fixed print & tuple

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,14 +15,12 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
                          db=MY_DB, port=PORT)
     cur = db.cursor()
-    query = "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') \
-                    FROM cities \
-                    INNER JOIN states \
-                    ON states.name = %s \
-                    WHERE cities.state_id = states.id \
-                    ORDER BY cities.id ASC;", (NAME,)
-    cur.execute(query)
-    row_query = cur.fetchall()
-    for rq_print in row_query:
-        print(rq_print)
+    cur.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') \
+                FROM cities \
+                INNER JOIN states \
+                ON states.name = %s \
+                WHERE cities.state_id = states.id \
+                ORDER BY cities.id ASC;", (NAME,))
+    row_query = cur.fetchone()
+    print(row_query[0] if row_query is not None else "")
 db.close()
