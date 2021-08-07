da949a484eb62c17b90d5c15b6814d3abac731c7
Miguel22247 <nr2862002@gmail.com>
2021-08-07 18:04:53 -0300

    added 12 and 13

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A script that lists all states"""
+
+if __name__ == '__main__':
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    if len(argv) != 4:
+        print("error")
+    else:
+        USER = argv[1]
+        PASS = argv[2]
+        DB = argv[3]
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(USER, PASS, DB), pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        update_state = session.query(State).filter(State.id == 2).first()
+        update_state.name = 'New Mexico'
+        session.commit()
