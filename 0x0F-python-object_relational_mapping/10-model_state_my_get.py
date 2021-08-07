96f4207f597aeecc3e899a0d28a03cd1a49bd33c
Miguel22247 <nr2862002@gmail.com>
2021-08-07 15:24:57 -0300

    fixed pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,9 +18,9 @@ if __name__ == '__main__':
                                .format(USER, PASS, DB), pool_pre_ping=True)
         Session = sessionmaker(bind=engine)
         session = Session()
-        state = session.query(State).filter(State.name == NAME).first()
-        if state is not None:
-            print("{}".format(state.id))
+        state_id = session.query(State).filter(State.name == NAME).first()
+        if state_id is not None:
+            print("{}".format(state_id.id))
         else:
             print("Not found")
         session.close()
