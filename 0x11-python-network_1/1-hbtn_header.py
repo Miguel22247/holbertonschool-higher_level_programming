af481e9f4c72f5af9e71b44d30c823bd0e33f8f1
Miguel22247 <minipachru@gmail.com>
2021-08-20 13:53:48 -0300

    fixed prints

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 
 if __name__ == '__main__':
     with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.getheader('X-Request-Id'))
+        print("{}".format(response.getheader('X-Request-Id')))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,4 +8,4 @@ import sys
 if __name__ == '__main__':
     email_value = {'email': sys.argv[2]}
     email_r = requests.post(sys.argv[1], data=email_value)
-    print(email_r.text)
+    print("{}".format(email_r.text))
