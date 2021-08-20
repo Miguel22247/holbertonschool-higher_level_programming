98881a2b787ccd81414e97144a613dab3050114f
Miguel22247 <minipachru@gmail.com>
2021-08-20 12:45:10 -0300

    add 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """script that fetches"""
+
+
 import requests
-
-
 if __name__ == "__main__":
     request = requests.get('https://intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}"
