9de6744d7115c2114bed9fd7ba9c6e7e44d0e147
Miguel22247 <minipachru@gmail.com>
2021-08-20 14:30:30 -0300

    add 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Script that takes in a letter and send it """
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+values = {'q' : q}
+request = requests.post('http://0.0.0.0:5000/search_user', data=values)
+try:
+    response = request.json()
+    if response:
+        print("[{}] {}".format(response['id'], response['name']))
+    else:
+        print("No result")
+except:
+    print("Not a valid JSON")
