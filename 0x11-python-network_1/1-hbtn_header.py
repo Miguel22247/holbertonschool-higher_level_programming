aaff0db3a03f3665d3299be0c678d6ee0f17aa69
Miguel22247 <minipachru@gmail.com>
2021-08-20 09:47:54 -0300

    add 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" takes a URL, sends a request and returns"""
+
+
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+	with urllib.request.urlopen(sys.argv[1]) as response:
+		print(response.getheader('X-Request-Id'))
