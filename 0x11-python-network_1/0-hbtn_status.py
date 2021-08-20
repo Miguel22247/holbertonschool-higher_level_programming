6037d91f8e8ecb7249267e038f2b173d4331d98f
Miguel22247 <minipachru@gmail.com>
2021-08-20 09:01:50 -0300

    add 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python code that check intranet status"""
+
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    status = response.read().decode('utf-8')
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: \
+{}".format(type(status), status, status.decode))
