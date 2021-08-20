207fbaeeaf7798af0656e925cae57ab5c6a9a7f4
Miguel22247 <minipachru@gmail.com>
2021-08-20 10:23:38 -0300

    add 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that post an email"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email_value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(email_value)
+    data = data.encode('utf-8')
+    request = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
