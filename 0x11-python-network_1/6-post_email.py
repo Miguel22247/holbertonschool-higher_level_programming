42495a090f4b84b8e59900a35f280682be9cef01
Miguel22247 <minipachru@gmail.com>
2021-08-20 13:08:37 -0300

    add 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that post an email"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    email_value = {'email': sys.argv[2]}
+    email_r = requests.post(sys.argv[1], data=email_value)
+    print(email_r.text)
