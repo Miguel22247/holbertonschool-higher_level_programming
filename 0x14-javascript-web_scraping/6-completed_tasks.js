2ad8b960a08859770d550334846ef3f6a0b9fe58
Miguel22247 <minipachru@gmail.com>
2021-09-14 13:00:07 -0300

    add 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
+const url = argv[2];
+const request = require('request');
+request(url, function (error, response, body) {
+	if (error) {
+		console.log(error)
+	}
+});
