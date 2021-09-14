7bf7d2554a10fb0b114c649e2104e857df0f779b
Miguel22247 <minipachru@gmail.com>
2021-09-14 13:25:09 -0300

    change variable place

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,12 +2,12 @@
 const argv = process.argv;
 const url = argv[2];
 const request = require('request');
+const dict = {};
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const dict = {};
     const results = JSON.parse(body);
     for (let i = 0; i < results.length; i++) {
       if (results[i].completed === true) {
