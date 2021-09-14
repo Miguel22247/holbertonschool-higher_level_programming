d362ee114bb4d40afc78f40bff66c984cbad20cf
Miguel22247 <minipachru@gmail.com>
2021-09-14 10:54:35 -0300

    add 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const argv = process.argv;
+const num = argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + num;
+const request = require('request');
+request(url, function (error, response, body) {
+	if (error) {
+	  console.log(error);
+	} else {
+	  const info = JSON.parse(body);
+	  console.log(info.title);
+	}
+  });
