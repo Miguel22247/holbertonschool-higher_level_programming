f26bcb6ab6ff111e20a9aecfdd0c8885f7f594af
Miguel22247 <minipachru@gmail.com>
2021-09-14 10:12:41 -0300

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 const argv = process.argv
 const fs = require('fs')
 fs.readFile(argv[2], 'utf-8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const argv = process.argv
 const fs = require('fs')
 fs.writeFile(argv[2], argv[3], function (err) {
-    if (err) {
-        return console.log(err)
-    }
+  if (err) {
+  return console.log(err)
+  }
 });
