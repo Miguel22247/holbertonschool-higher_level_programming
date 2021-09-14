02efaa03fbbaf5340c12e686ddc3f2c211692aea
Miguel22247 <minipachru@gmail.com>
2021-09-14 09:46:18 -0300

    add 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = process.argv
+const fs = require('fs')
+fs.readFile(argv[2], 'utf-8', function (err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	console.log(data);
+});
