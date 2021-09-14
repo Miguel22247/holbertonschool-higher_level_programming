eb36a5aa33f02446cdf5abe08ca2138ecf62b8ad
Miguel22247 <minipachru@gmail.com>
2021-09-14 13:26:49 -0300

    return to previous state

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,12 +3,12 @@ const argv = process.argv;
 const url = argv[2];
 const request = require('request');
 const dict = {};
-const results = JSON.parse(body);
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
+    const results = JSON.parse(body);
     for (let i = 0; i < results.length; i++) {
       if (results[i].completed === true) {
         if (results[i].userId in dict) {
