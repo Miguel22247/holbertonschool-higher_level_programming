4213b34cbd4fb6e814ffdaf18c116c9aa2a20056
Miguel22247 <minipachru@gmail.com>
2021-09-14 12:10:35 -0300

    fix semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@ const argv = process.argv;
 const fs = require('fs');
 fs.writeFile(argv[2], argv[3], function (err) {
   if (err) {
-  return console.log(err);
+    return console.log(err);
   }
 });
