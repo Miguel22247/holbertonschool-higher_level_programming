f3493a085c16a08eaadae2998ccb97e105c8d30d
Miguel22247 <minipachru@gmail.com>
2021-09-14 12:09:01 -0300

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const argv = process.argv
-const fs = require('fs')
+const argv = process.argv;
+const fs = require('fs');
 fs.readFile(argv[2], 'utf-8', function (err, data) {
   if (err) {
     return console.log(err);
