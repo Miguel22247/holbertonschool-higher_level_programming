4bee19f2a610fcc77c48229e7b8d6829173cb33a
Miguel22247 <minipachru@gmail.com>
2021-09-14 12:38:40 -0300

    fix things

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const argu = process.argv;
+const argv = process.argv;
 const fs = require('fs');
 const request = require('request');
 
-request(argu[2], function (error, response, body) {
+request(argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(argu[3], body, function (err) {
+    fs.writeFile(argv[3], body, function (err) {
       if (err) {
         return console.log(err);
       }
