087f481b062320c19b15e73841fd539e364713d9
Miguel22247 <minipachru@gmail.com>
2021-09-14 13:06:37 -0300

    add 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,8 +2,22 @@
 const argv = process.argv;
 const url = argv[2];
 const request = require('request');
+const results = JSON.parse(body);
+const dict = {};
+
 request(url, function (error, response, body) {
   if (error) {
-	console.log(error)
+    console.log(error)
+  } else {
+    for (let i = 0; i < results.length; i++) {
+        if (results[i].completed === true) {
+          if (results[i].userId in dict) {
+            dict[results[i].userId] += 1;
+          } else {
+            dict[results[i].userId] = 1;
+          }
+        }
+      }
+      console.log(dict);
   }
 });
