5fb1455c15fbdd5aabe5b4bf9d66099047846c4a
Miguel22247 <minipachru@gmail.com>
2021-09-14 10:33:16 -0300

    fix 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const argv = process.argv
-const fs = require('fs')
+const argv = process.argv;
+const fs = require('fs');
 fs.writeFile(argv[2], argv[3], function (err) {
   if (err) {
-  return console.log(err)
+  return console.log(err);
   }
 });
