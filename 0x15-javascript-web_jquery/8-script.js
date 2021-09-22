737a2dfca86520465187c90d705a7cd8eef527f7
Miguel22247 <minipachru@gmail.com>
2021-09-22 11:23:09 -0300

    add 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function(data){
+    $.each(data.results, function(i, movie){
+        $("UL#list_movies").append("<li>" + movie.title + "</li>");
+      });
+});
